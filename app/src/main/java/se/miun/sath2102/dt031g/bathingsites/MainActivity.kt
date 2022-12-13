dbc55616101724795d1f54ca1ee32e25e79e7824
Samuel Thand <sath2102@student.miun.se>
2022-12-13 16:23:58 +0100

    Added main menu with settings item

--- app/src/main/java/se/miun/sath2102/dt031g/bathingsites/MainActivity.kt
+++ app/src/main/java/se/miun/sath2102/dt031g/bathingsites/MainActivity.kt
@@ -2,10 +2,22 @@ package se.miun.sath2102.dt031g.bathingsites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import se.miun.sath2102.dt031g.bathingsites.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
