c97cd7767a835867676a903b1b7453817c6ac487
Samuel Thand <sath2102@student.miun.se>
2022-12-14 18:19:46 +0100

    Started working on menu bar for AddBathingSiteFragment

--- app/src/main/java/se/miun/sath2102/dt031g/bathingsites/AddBathingSiteFragment.kt
+++ app/src/main/java/se/miun/sath2102/dt031g/bathingsites/AddBathingSiteFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import se.miun.sath2102.dt031g.bathingsites.databinding.FragmentAddBathingSiteBinding
+import se.miun.sath2102.dt031g.bathingsites.databinding.FragmentBathingSitesBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,9 +22,11 @@ class AddBathingSiteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentAddBathingSiteBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -33,8 +37,10 @@ class AddBathingSiteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_bathing_site, container, false)
+
+        binding = FragmentAddBathingSiteBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     companion object {
